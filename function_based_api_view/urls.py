b4b21ba9911b2c6281558d51e82d827e894c3991
Aashishkumar123 <aashishkumar12376@gmail.com>
2021-07-24 12:50:13 +0530

    sljfj

--- function_based_api_view/urls.py
+++ function_based_api_view/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('student-data/',views.StudentView,name="stuAPI"),
+    path('student-data/<int:id>/',views.StudentViewUD,name="stuAPIUD"),
 ]
--- function_based_api_view/views.py
+++ function_based_api_view/views.py
@@ -1,12 +1,45 @@
-from rest_framework import serializers
+import re
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Student
 from .serializers import StudentSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def StudentView(request):
-    stu = Student.objects.all()
-    serializer = StudentSerializer(stu,many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu,many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        data = request.data
+        serializer = StudentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+            
+
+
+@api_view(['PUT','DELETE'])
+def StudentViewUD(request,id):
+    try:
+        student = Student.objects.get(pk=id)
+    except Student.DoesNotExist:
+        return Response("id not found")
+
+    if request.method == "PUT":
+        data = request.data
+        serializer = StudentSerializer(student,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":'Data Updated'})
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        Student.objects.get(id=id).delete()
+        return Response({"msg":'Data Deleted'})
+           
